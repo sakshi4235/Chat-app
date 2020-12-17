3bc3e44e4fb451ba4c5922d192581573102ffdcb
sakshi4235 <65545679+sakshi4235@users.noreply.github.com>
2020-12-17 23:34:35 +0530

    Create index.js

---
+++ nodeserver/index.js
@@ -0,0 +1,25 @@
+//node server which will handle socket io connections
+var app=require('express')();
+var http=require('http').Server(app);
+var io=require('socket.io')(http);
+
+const users={};
+
+io.on('connection',(socket) =>{
+    socket.on('new-user-joined',name =>{
+        console.log("New user",name)
+        users[socket.id]=name;
+        socket.broadcast.emit('user-joined',name);
+    });
+    socket.on('send' ,(message)=>{
+        socket.broadcast.emit('receive',{message:message,name:users[socket.id]})
+    });
+    socket.on('disconnect' ,(message)=>{
+        socket.broadcast.emit('left',users[socket.id]);
+        delete users[socket.id];
+    });
+})
+http.listen(8000,function(){
+    console.log('listening');
+})
+
